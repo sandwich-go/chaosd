29690c64b240319471d0d916e29137aac6b7c8fb
zhengyang.zhu <zhengyang.zhu@centurygame.com>
2024-09-09 17:06:42 +0800

    test: add upload

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 rm -rf ./bin
 make chaosd
 make chaos-tools
-mv bin chaosd-v1.4.1-linux-amd64
-tar -czvf chaosd-v1.4.1-linux-amd64.tar.gz chaosd-v1.4.1-linux-amd64
-protokitgo upload --source_files=chaosd-v1.4.1-linux-amd64.tar.gz --dest_dsn=oss@/chaosd/@pmt_setting@OSS_8 --config=setting.yaml --no_append_len --no_append_md5
+mv bin chaosd-linux-amd64
+tar -czvf chaosd-linux-amd64.tar.gz chaosd-v1.4.2-linux-amd64
+protokitgo upload --source_files=chaosd-v1.4.2-linux-amd64.tar.gz --dest_dsn=oss@/chaosd/@pmt_setting@OSS_8 --config=setting.yaml --no_append_len --no_append_md5
